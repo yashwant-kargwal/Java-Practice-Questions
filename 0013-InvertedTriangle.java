a2610e2fcdb6bfbd3793c14476b9d7c9ba9e7b6d
Yashwant Kargwal <154042689+yashwant-kargwal@users.noreply.github.com>
2025-01-24 01:34:10 +0530

    Create 0013-InvertedTriangle.java

---
+++ 0013-InvertedTriangle.java
@@ -0,0 +1,26 @@
+// Creator - Yashwant Kargwal
+// Don't Forget to ⭐Star and Fork.
+// Problem: Create an inverted right-angled triangle pattern with *.
+
+// Output of Pattern :- 
+// *****
+// ****
+// ***
+// **
+// *
+
+class Main{
+    public static void main(String[]args){
+        
+        // this loop is used to print the line of pattern
+        for(int i = 5; i >=1; i--){
+            // this loop is used to print the *
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            // this is used for next line
+            System.out.println();
+        }
+        
+    }
+}
