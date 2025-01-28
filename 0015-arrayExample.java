5c188f0252c4eff49e28b070a003a6157fbd46cb
Yashwant Kargwal <154042689+yashwant-kargwal@users.noreply.github.com>
2025-01-28 13:06:59 +0530

    Create 0015-arrayExample.java

---
+++ 0015-arrayExample.java
@@ -0,0 +1,24 @@
+// Yashwant Kargwal
+// Don't Forget to ⭐Star and Fork.
+// Basic Example of array in java
+
+public class Main {
+    public static void main(String[]args){
+
+        // declare
+        int array[];
+        
+        // memory allocation
+        array = new int[5];
+
+        // value add
+        array[0] = 12;
+        array[1] = 24;
+        array[2] = 24;
+        array[3] = 24;
+        array[4] = 24;
+
+        // print 
+        System.out.println(array[0]+" "+array[1]);
+    }
+}
